9af3de0596d5bddbda1cf7fc6ad679bee1c54ad0
Ahmed Fathi <ahmed.fathi@AssetDev.com>
2023-12-19 21:10:49 +0200

    import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingInterceptor } from './core/inspectors/loading-interceptor';
 import { AppComponent } from './app.component';
+import {CoreModule} from './core/core.module'
+
 @NgModule({
   declarations: [
     AppComponent
